592e963e62d58a59527093fab5a370ca44f188f2
hxfycy <felixhuxf@gmail.com>
2020-05-16 12:06:08 +0800

    sha256 dataflow added

--- sha256/sha256_plainC.cpp
+++ sha256/sha256_plainC.cpp
@@ -13,8 +13,6 @@ unsigned long a[8] = { 0 };
 #define CH(x,y,z) (((x) & (y)) ^ (~(x) & (z)))
 #define MAJ(x,y,z) (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
 
-#define BSIG0(x) (ROTRIGHT(x,7) ^ ROTRIGHT(x,18) ^ ((x) >> 3))
-#define BSIG1(x) (ROTRIGHT(x,17) ^ ROTRIGHT(x,19) ^ ((x) >> 10))
 
 #define EP0(x) (ROTRIGHT(x,2) ^ ROTRIGHT(x,13) ^ ROTRIGHT(x,22))
 #define EP1(x) (ROTRIGHT(x,6) ^ ROTRIGHT(x,11) ^ ROTRIGHT(x,25))
